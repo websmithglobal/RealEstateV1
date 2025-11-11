12375f7bc4000c1496dccc9e22f0d60c6095298d
NirmalWebsmith <nirmal@websmithsolution.com>
2025-11-11 14:56:15 +0530

    Add Login page 11.11.2025 Nirmal

---
+++ RealEstate.Application/DTOs/LoginDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Application.DTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+    }
+}
